64ac2fc4d9089a9e12c124fce27ec495fa8bd731
Denis Doda <112046982+dodasbb@users.noreply.github.com>
2025-07-23 16:47:28 -0400

    Updates to portfolio and resume

--- languages.js
+++ languages.js
@@ -1,91 +1,109 @@
 const languages = {
-    Frontend: [
-        {
-            name: 'HTML5',
-            imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/240px-HTML5_Badge.svg.png',
-            alt: 'HTML5 Logo',
-        },
-        {
-            name: 'CSS3',
-            imgSrc: 'https://cdn.iconscout.com/icon/free/png-256/css-131-722685.png',
-            alt: 'CSS3 Logo',
-        },
-        {
-            name: 'Tailwind CSS',
-            imgSrc: './assets/tailwindlogo.png',
-            alt: 'Tailwind Logo',
-        },
-        {
-            name: 'JavaScript',
-            imgSrc: 'https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png',
-            alt: 'JavaScript Logo',
-        },
+    "Languages": [
         {
             name: 'TypeScript',
-            imgSrc: 'https://cdn-icons-png.flaticon.com/512/5968/5968381.png',
+            imgSrc: '/assets/TechIcons/typescript.png',
             alt: 'TypeScript Logo',
         },
         {
-            name: 'React.js',
-            imgSrc: 'https://cdn.iconscout.com/icon/free/png-256/react-3-1175109.png',
-            alt: 'React.js Logo',
-        },
-        {
-            name: 'React Native',
-            imgSrc: 'https://cdn.worldvectorlogo.com/logos/react-native-1.svg',
-            alt: 'React Native Logo',
-        },
-        {
-            name: 'Next.js',
-            imgSrc: 'https://www.svgrepo.com/show/354113/nextjs-icon.svg',
-            alt: 'Next.js Logo',
-        },
-    ],
-    Backend: [
-        {
-            name: 'Node.js',
-            imgSrc: 'https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png',
-            alt: 'Node.js Logo',
-        },
-        {
-            name: 'PostgreSQL',
-            imgSrc: 'https://static-00.iconduck.com/assets.00/postgresql-icon-1987x2048-v2fkmdaw.png',
-            alt: 'PostgreSQL Logo',
-        },
-        {
-            name: 'Prisma',
-            imgSrc: 'https://static-00.iconduck.com/assets.00/prisma-icon-423x512-isxs6996.png',
-            alt: 'Prisma Logo',
-        },
-        {
-            name: 'Docker',
-            imgSrc: 'https://cdn-icons-png.flaticon.com/512/919/919853.png',
-            alt: 'Docker Logo',
-        },
-        {
-            name: 'Firebase',
-            imgSrc: 'https://cdn.iconscout.com/icon/free/png-512/free-firebase-3521427-2944871.png?f=webp&w=256',
-            alt: 'Firebase Logo',
+            name: 'JavaScript',
+            imgSrc: '/assets/TechIcons/javascript.svg',
+            alt: 'JavaScript Logo',
         },
         {
             name: 'Python',
-            imgSrc: 'https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png',
+            imgSrc: '/assets/TechIcons/python.webp',
             alt: 'Python Logo',
         },
         {
             name: 'Java',
-            imgSrc: 'https://cdn.iconscout.com/icon/free/png-512/free-java-22-225997.png?f=webp&w=256',
+            imgSrc: '/assets/TechIcons/java.webp',
             alt: 'Java Logo',
         },
         {
+            name: 'C',
+            imgSrc: '/assets/TechIcons/c.webp',
+            alt: 'C Logo',
+        },
+        {
             name: 'C++',
-            imgSrc: 'https://cdn-icons-png.flaticon.com/512/6132/6132222.png',
+            imgSrc: '/assets/TechIcons/cpp.png',
             alt: 'C++ Logo',
         },
         {
-            name: 'C',
-            imgSrc: 'https://cdn.iconscout.com/icon/free/png-512/free-c-57-1175191.png?f=webp&w=256',
-            alt: 'C Logo',
+            name: 'HTML5',
+            imgSrc: '/assets/TechIcons/html5.png',
+            alt: 'HTML5 Logo',
         },
+        {
+            name: 'CSS3',
+            imgSrc: '/assets/TechIcons/css3.webp',
+            alt: 'CSS3 Logo',
+        },
+        
+        
+        
+        
+    ],
+    "Frameworks": [
+        {
+            name: 'React.js',
+            imgSrc: '/assets/TechIcons/react.webp',
+            alt: 'React.js Logo',
+        },
+        {
+            name: 'React Native',
+            imgSrc: '/assets/TechIcons/reactnative.svg',
+            alt: 'React Native Logo',
+        },
+        {
+            name: 'Node.js',
+            imgSrc: '/assets/TechIcons/nodejs.svg',
+            alt: 'Node.js Logo',
+        },
+        {
+            name: 'Next.js',
+            imgSrc: '/assets/TechIcons/nextjs.svg',
+            alt: 'Next.js Logo',
+        },
+        {
+            name: 'Prisma',
+            imgSrc: '/assets/TechIcons/prisma.svg',
+            alt: 'Prisma Logo',
+        },
+        {
+            name: 'Tailwind CSS',
+            imgSrc: '/assets/TechIcons/tailwind.png',
+            alt: 'Tailwind Logo',
+        },
+    ],
+    "Cloud & Tools": [
+        {
+            name: 'AWS',
+            imgSrc: '/assets/TechIcons/aws.svg',
+            alt: 'AWS Logo',
+        },
+        {
+            name: 'Docker',
+            imgSrc: '/assets/TechIcons/docker.png',
+            alt: 'Docker Logo',
+        },
+        {
+            name: 'PostgreSQL',
+            imgSrc: '/assets/TechIcons/postgres.svg',
+            alt: 'PostgreSQL Logo',
+        },
+        {
+            name: 'Firebase',
+            imgSrc: '/assets/TechIcons/firebase.webp',
+            alt: 'Firebase Logo',
+        },
+        {
+            name: 'Google Analytics',
+            imgSrc: '/assets/TechIcons/googleanalytics.svg',
+            alt: 'Google Analytics Logo',
+        },
+        
+        
     ],
 };
